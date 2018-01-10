860d5634ebb80e66499a10d49428a0b302972605
matukai <matt.tanji@gmail.com>
2018-01-10 12:31:29 -1000

    a

--- context.js
+++ context.js
@@ -17,14 +17,13 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0,1));
 
 // #2  
-console.log(  );
+console.log(add.call(voltron,2,3));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus,20,30));
 
 // #4
-
-console.log(  );
+console.log(add.bind(calculate,bender), calculate(drinking,beer));
